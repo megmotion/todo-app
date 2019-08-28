967dc58102e2462288a237221f17ecb310061559
megmotion <33497517+megmotion@users.noreply.github.com>
2019-08-29 01:22:47 +0200

    add delete button, submit on enter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,19 @@ export class AppComponent {
   todoArray=[];
   addTodo(value){    
   	this.todoArray.push(value)    
-  	console.log(this.todo)  
+  }
+  deleteItem(todo){   
+  	for(let i=0 ;i<= this.todoArray.length ;i++){    
+  		if(todo== this.todoArray[i]){     
+  			this.todoArray.splice(i,1)    
+  		}   
+  	}
+  }
+  todoSubmit(value:any){ 
+  	if(value !== ""){    
+  		this.todoArray.push(value.todo)        
+  	} else {      
+  		alert('Field required **')    
+  	}       
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+  	BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
